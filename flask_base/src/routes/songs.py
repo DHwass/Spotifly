097b58392e608f46198dded83d5499066f65f96a
AmineHad <amine.hadid30@gmail.com>
2024-01-02 23:31:26 +0100

    Added AddSong and modify song

--- flask_base/src/routes/songs.py
+++ flask_base/src/routes/songs.py
@@ -80,3 +80,96 @@ def get_songs():
     """
     return songs_service.get_songs()
 
+@songs.route('/', methods=['POST'])
+#@login_required
+def post_song():
+    """
+    ---
+    post:
+      description: Creating a song
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: SongCreate
+      responses:
+        '201':
+          description: Created
+          content:
+            application/json:
+              schema: Song
+            application/yaml:
+              schema: Song
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '422':
+          description: Unprocessable entity
+          content:
+            application/json:
+              schema: UnprocessableEntity
+            application/yaml:
+              schema: UnprocessableEntity
+      tags:
+          - songs
+    """
+    print(request.json, "this is from route")
+    return songs_service.create_song(request.json)
+
+@songs.route('/<id>', methods=['PUT'])
+#@login_required
+def put_song(id):
+    """
+    ---
+    put:
+      description: Updating a song
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: SongUpdate
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Song
+            application/yaml:
+              schema: Song
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+        '422':
+          description: Unprocessable entity
+          content:
+            application/json:
+              schema: UnprocessableEntity
+            application/yaml:
+              schema: UnprocessableEntity
+      tags:
+          - songs
+    """
+    return songs_service.update_song(id, request.json)
+
--- flask_base/src/services/songs.py
+++ flask_base/src/services/songs.py
@@ -18,3 +18,33 @@ def get_song(id):
 def get_songs():
     response = requests.request(method="GET", url=songs_url)
     return response.json(), response.status_code
+
+def create_song(song_register):
+    # on récupère le schéma song pour la requête vers l'API users
+    song_schema = SongSchema().loads(json.dumps(song_register), unknown=EXCLUDE)
+    print("this is from service ",song_schema)
+    # on crée song côté API users
+    response = requests.request(method="POST", url=songs_url, json=song_schema)
+
+    if response.status_code != 200:
+        return response.json(), response.status_code
+
+  
+
+    return response.json(), response.status_code
+
+def update_song(id, song_update):
+
+
+    # s'il y a quelque chose à changer côté API 
+    song_schema = SongSchema().loads(json.dumps(song_update), unknown=EXCLUDE)
+    print(song_schema)
+    response = None
+    if not SongSchema.is_empty(song_schema):
+        # on lance la requête de modification
+        response = requests.request(method="PUT", url=songs_url+id, json=song_schema)
+        print(response.status_code)
+        if response.status_code != 200:
+            return response.json(), response.status_code
+
+    return response.json(), response.status_code
