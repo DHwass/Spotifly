ff74750cfb1a499967b25d13e8ef8d3f33bf8773
AmineHad <amine.hadid30@gmail.com>
2024-01-10 20:37:37 +0100

    moved ratingsfile into songsfiles so it can be on file and added modify and get a rating

--- flask_base/src/routes/songs.py
+++ flask_base/src/routes/songs.py
@@ -208,4 +208,196 @@ def delete_song(id):
           - songs
     """
     return songs_service.delete_song(id)
+#after having done everything for the songs, we will do the same for the ratings of the songs
+@songs.route('/<id>/ratings', methods=['GET'])
+#@login_required
+def get_ratings(id):
+    """
+    ---
+    get:
+      description: Getting all ratings of a song
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Ratings
+            application/yaml:
+              schema: Ratings
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+      tags:
+          - ratings
+    """
+    return songs_service.get_ratings(id)
+  
+@songs.route('/<id>/ratings', methods=['POST'])
+def add_rating(id):
+        """
+        ---
+        post:
+          description: Creating a rating
+          parameters:
+            - in: path
+              name: id
+              schema:
+                type: uuidv4
+              required: true
+              description: UUID of song id
+          requestBody:
+            required: true
+            content:
+                application/json:
+                    schema: RatingCreate
+          responses:
+            '201':
+              description: Created
+              content:
+                application/json:
+                  schema: Rating
+                application/yaml:
+                  schema: Rating
+            '401':
+              description: Unauthorized
+              content:
+                application/json:
+                  schema: Unauthorized
+                application/yaml:
+                  schema: Unauthorized
+            '422':
+              description: Unprocessable entity
+              content:
+                application/json:
+                  schema: UnprocessableEntity
+                application/yaml:
+                  schema: UnprocessableEntity
+          tags:
+              - ratings
+        """
+        return songs_service.create_rating(id, request.json)
+
+@songs.route('/<id>/ratings/<id_rating>', methods=['GET'])
+#@login_required
+def get_rating(id,id_rating):
+    """
+    ---
+    get:
+      description: Getting a rating of a song
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+        - in: path
+          name: id_rating
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of rating id
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Rating
+            application/yaml:
+              schema: Rating
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+      tags:
+          - ratings
+    """
+    return songs_service.get_rating(id,id_rating)
+@songs.route('/<id>/ratings/<id_rating>', methods=['PUT'])
+#@login_required
+def update_rating(id,id_rating):
+    """
+    ---
+    put:
+      description: Updating a rating
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+        - in: path
+          name: id_rating
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of rating id
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: RatingUpdate
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Rating
+            application/yaml:
+              schema: Rating
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+        '422':
+          description: Unprocessable entity
+          content:
+            application/json:
+              schema: UnprocessableEntity
+            application/yaml:
+              schema: UnprocessableEntity
+      tags:
+          - ratings
+    """
+    return songs_service.update_rating(id,id_rating, request.json)
+  
 
--- flask_base/src/services/songs.py
+++ flask_base/src/services/songs.py
@@ -1,8 +1,10 @@
 import json
+import uuid
 import requests
 from sqlalchemy import exc
 from marshmallow import EXCLUDE
 from flask_login import current_user
+from src.schemas.rating import RatingSchema
 
 from src.schemas.song import SongSchema
 from src.models.http_exceptions import *
@@ -54,4 +56,57 @@ def delete_song(id):
     response = requests.request(method="DELETE", url=songs_url+id)
     return  response.status_code
     
+def get_ratings(id):
+    response = requests.request(method="GET", url="https://ratings-alpha.edu.forestier.re/songs/"+id+"/ratings")
+    return response.json(), response.status_code
 
+def create_rating(id,rating_register):
+  
+    # on récupère le schéma rating pour la requête vers l'API ratings
+    rating_schema = RatingSchema().loads(json.dumps(rating_register), unknown=EXCLUDE)
+    print("here",rating_schema)
+    #generation d'un uuid
+    new_uuid = str(uuid.uuid4())
+
+    rating_schema["id"]=new_uuid
+    rating_schema["user_id"] = current_user.id
+    rating_schema["rating_date"] = "2023/12/12"  
+    rating_schema["song_id"] = id
+    
+    print(rating_schema)
+    #check if music_id exists
+    songs, status_code = get_song(id)
+  
+
+    if status_code != 200:
+        return songs, status_code
+    
+    # on crée le rating côté API ratings
+    response = requests.request(method="POST", url="https://ratings-alpha.edu.forestier.re/songs/"+id+"/ratings", json=rating_schema)
+     
+    print(response.json())
+
+    if response.status_code != 201:
+        return response.json(), response.status_code
+
+  
+
+    return response.json(), response.status_code
+
+def get_rating(id,rating_id):
+    response = requests.request(method="GET", url="https://ratings-alpha.edu.forestier.re/songs/"+id+"/ratings/"+rating_id)
+    return response.json(), response.status_code
+
+def update_rating(id,rating_id,rating_update):
+    # s'il y a quelque chose à changer côté API 
+    rating_schema = RatingSchema().loads(json.dumps(rating_update), unknown=EXCLUDE)
+    print(rating_schema)
+    response = None
+    if not RatingSchema.is_empty(rating_schema):
+        # on lance la requête de modification
+        response = requests.request(method="PUT", url="https://ratings-alpha.edu.forestier.re/songs/"+id+"/ratings/"+rating_id, json=rating_schema)
+        print(response.status_code)
+        if response.status_code != 200:
+            return response.json(), response.status_code
+
+    return response.json(), response.status_code
